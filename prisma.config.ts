eb91b12711b240e7da36eef51c9ebe38776fe942
RonoaMad1 <mniakate4@myges.fr>
2026-05-02 22:05:20 +0200

    fix: add datasource url to prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,16 @@
 import path from 'path'
 import { defineConfig } from 'prisma/config'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 export default defineConfig({
   earlyAccess: true,
   schema: path.join('prisma', 'schema.prisma'),
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
   migrate: {
     async adapter() {
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      const connectionString = process.env.DATABASE_URL
-      return new PrismaPg({ connectionString })
+      return new PrismaPg({ connectionString: process.env.DATABASE_URL })
     }
   }
 })
